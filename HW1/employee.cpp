a6fcd336a99f55a190d10462bde43ad8924cad11
goggalor1954 <goggalor1954@gmail.com>
2017-02-02 16:41:28 -0500

    work it

---
+++ HW1/employee.cpp
@@ -0,0 +1,24 @@
+//James Roesemann
+//Assignment 1 Basic Class
+#include <iostream>
+using namespace std;
+
+class employee
+{
+	public:
+		
+	private:
+	double SALARY;
+	int ID;
+}
+
+
+Int main()
+{
+
+	
+	
+	
+
+	return 0;
+}
