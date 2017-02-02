a1f4e6ae2ea649db9682783ae561ce790a4995cf
goggalor1954 <goggalor1954@gmail.com>
2017-02-02 17:36:50 -0500

    about to go to class

--- HW1/employee.cpp
+++ HW1/employee.cpp
@@ -1,19 +1,37 @@
 //James Roesemann
 //Assignment 1 Basic Class
+//This program declares an instance of the class "employee" named Tom, prompts the 
+//user to enter both Salary and id and assignes those values to SALARY and ID in the 
+//object Tom.
 #include <iostream>
 using namespace std;
 
-class employee
+class Employee
 {
 	public:
+		Employee(void); //constructor, sets both variables to zero.
+		void setSalary(float newSalary); // sets the value of SALARY
+		void setID(int newID); // sets the value of SALARY
+		float getSalary(void); //returns the value of SALARY
+		int getID(void); //returns the value of ID 
+
 		
 	private:
 	double SALARY;
 	int ID;
+};
+
+Employee::Employee(void) //constructor, sets both variables to zero.
+{
+	ID=0;
+	SALARY=0;
 }
+float Employee::getSalary(void) {return SALARY;} //returns the value of SALARY
+int Employee::getID(void) {return ID;} //returns the value of ID 
+void Employee::setSalary(float newSalary) { SALARY=newSalary; } // sets the value of SALARY
+void Employee::setID(int newID) {ID=newID;} // sets the value of SALARY
 
-
-Int main()
+int main()
 {
 
 	
