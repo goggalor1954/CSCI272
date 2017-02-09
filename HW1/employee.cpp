c43f56006b1c637fa9120bcaf356a0267e5680e8
James Roesemann <goggalor1954@gmail.com>
2017-02-08 20:47:16 -0500

    Done with homework 1, submiting

--- HW1/employee.cpp
+++ HW1/employee.cpp
@@ -1,8 +1,6 @@
 //James Roesemann
 //Assignment 1 Basic Class
-//This program declares an instance of the class "employee" named Tom, prompts the 
-//user to enter both Salary and id and assignes those values to SALARY and ID in the 
-//object Tom.
+/*This program declares an instance of the class "employee" named staffOne, prompts the user to enter both Salary and id and assignes those values to SALARY and ID in the object staffOne.*/
 #include <iostream>
 using namespace std;
 
@@ -15,13 +13,11 @@ class Employee
 		void setID(int newID); // sets the value of ID
 		float getSalary(void); //returns the value of SALARY
 		int getID(void); //returns the value of ID 
-
 		
 	private:
 	float SALARY;
 	int ID;
 };
-
 
 
 int main()
@@ -30,29 +26,31 @@ Employee staffOne;
 int staffID;
 float staffSal;
 
-std::cout << "Enter the ID Number and Salary of employee one." << endl;
+std::cout << "Enter the ID Number and Salary of the employee." << endl;
 std::cin >> staffID >> staffSal;
 staffOne.setID(staffID);
 staffOne.setSalary(staffSal);
-std::cout << "The ID of employee one is: " << staffOne.getID() << " and the salary is: " << staffOne.getSalary() << endl;
-std::cout << "Enter the ID Number and Salary of employee two." << endl;
+
+//That satisfies the Assignment. I wasen't sure how strictly you wanted to us to follow the instructions given. So I wrote the statments commented out below showing that the program works as intended and that the other constructor I wrote works as well.
+/*
+std::cout << "The ID of the employee is: " << staffOne.getID() << " and the salary is: " << staffOne.getSalary() << endl;
+std::cout << "Enter the ID Number and Salary of the employee." << endl;
 std::cin >> staffID >> staffSal;
-//fix this later
-EMployee staffTwo(staffID, staffSal);
-std::cout << "The ID of employee two is: " << staffTwo.getID() << " and the salary is: " << staffTwo.getSalary() << endl;
-	
+Employee staffTwo(staffID, staffSal);
+std::cout << "The ID of the employee is: " << staffTwo.getID() << " and the salary is: " << staffTwo.getSalary() << endl;
+*/	
 
 	return 0;
 }
 
 
 
-Employee::Employee(void) //constructor, sets both variables to zero.
+Employee::Employee(void) //default constructor, sets both variables to zero.
 {
 	ID=0;
 	SALARY=0;
 }
-Employee::Employee(int id, float salary)
+Employee::Employee(int id, float salary) //constructor. allows the user to create a employee objecte with both ID and SALARY
 {
 	ID=id;
 	SALARY=salary;
