ba95f8d2d4ffb50f44007d9e5f91ae4282b608a8
goggalor1954 <goggalor1954@gmail.com>
2017-02-14 17:33:19 -0500

    started HW3

---
+++ HW3/studentInfo.cpp
@@ -0,0 +1,3 @@
+//James Roesemann
+//Assignment 3 Derived class
+/* This program contains two classes. PERSONNEL contains the public data NAME and the private data ID. STUDENT is a class derived from PERSONAL that contains the private data MAJOR. This program declares an instance of each, and assigns values to each data member.
