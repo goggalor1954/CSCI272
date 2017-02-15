190e8ef978beda6ef5db62bca3d2e8beefd9f163
James Roesemann <goggalor1954@gmail.com>
2017-02-15 06:58:52 -0500

    worked on hw3 for a few mins

--- HW3/studentInfo.cpp
+++ HW3/studentInfo.cpp
@@ -1,3 +1,31 @@
 //James Roesemann
 //Assignment 3 Derived class
-/* This program contains two classes. PERSONNEL contains the public data NAME and the private data ID. STUDENT is a class derived from PERSONAL that contains the private data MAJOR. This program declares an instance of each, and assigns values to each data member.
+/* This program contains two classes. PERSONNEL contains the public data NAME and the private data ID. STUDENT is a class derived from PERSONAL that contains the private data MAJOR. This program declares an instance of each, and assigns values to each data member. */
+
+class PERSONNEL
+{
+	public:
+		PERSONNEL(void); //default constructor
+		PERSONNEL(string NAME, int ID); //constructor
+		string NAME;
+		void setName(string Name);//sets value of NAME
+		void setID(int ID); //sets value of ID
+		string getName(void); //returns value of NAME
+		int getID(void); //returns value of ID
+
+	private:
+		int ID;
+}
+
+
+int main()
+{
+
+	return 0;
+}
+
+void PERSONNEL::setName(string Name) {NAME=name;} //sets value of NAME
+void PERSONNEL::setID(int id) {ID=id;} //sets value of ID
+string PERSONNEL::getName(void) {return NAME;} //returns value of NAME
+int PERSONNEL::getID(void) {return ID;} //returns value of ID
+
