9706c2f1b949c7fdb59864f67a17945246e9af47
goggalor1954 <goggalor1954@gmail.com>
2017-03-30 14:41:57 -0400

    feel like i might have made progress. still stuck on this stupid problem though. can't figure out why when i set next it's pointing to another opbject. think its pointing to a copy.

--- HW4/nodex.cpp
+++ HW4/nodex.cpp
@@ -9,11 +9,12 @@ class NodeX
 {
 	public:
 	NodeX(void);
-	void setNext(NodeX nextPtr);
+	void setNext(NodeX*& nextPtr);
 	NodeX* getNext(void);
 	float getSalary();
 	void setSalary(float sal);
 	//testing
+	//NodeX& getNextA();
 
 	private:
 	NodeX *next;
@@ -21,27 +22,31 @@ class NodeX
 };
 
 NodeX::NodeX(void) {next=NULL;}
-void NodeX::setNext(NodeX nextPtr) {next=&nextPtr;}
+void NodeX::setNext(NodeX*& nextPtr) {next=nextPtr;}
 NodeX* NodeX::getNext(void) {return next;}
+//NodeX& NodeX::getNextA(void) {return *next;}
 float NodeX::getSalary() {return Salary;}
 void NodeX::setSalary(float sal) {Salary=sal;}
 
 int main()
 {
 	NodeX *thing1=new NodeX, *thing2=new NodeX;
-	/* TESTING
-	std::cout <<"address of thing 1 " << &thing1 << "\nand thing2 " <<  &thing2 << endl;
-	std::cout <<"next value for thing1 next " <<thing1.getNext() <<"\nand thing2 next " <<thing2.getNext() << endl;
-	thing1.setNodeX(&thing2);
-	std::cout <<"Now, Value of thing1 next " << thing1.getNext() <<"\nand thing2 next " <<thing2.getNext() <<endl;
-	std::cout <<"Does thing2.next()== NULL " << (thing2.getNext()==NULL) << true<< endl;
-	*/
-	thing1->setSalary(15.00);
+	thing1->setSalary(15.0);
 	thing2->setSalary(3.50);
-	std::cout << thing1->getSalary() <<"\n"<<thing2->getSalary() <<endl;
+	/*std::cout << thing1->getSalary() <<"\n"<<thing2->getSalary() <<endl;
 	
 	thing1->setNext(*thing2);
 	std::cout << thing1->getSalary() <<"\n" << thing1->getNext()->getSalary() << endl;
+	*/
+	//testing
+	std::cout <<&thing1 << " thin1g address"<< endl;;
+	std::cout <<&thing2 <<" thing2 address" << endl;
+	std::cout <<"Thing1 next " <<(*thing1).getNext() <<" thing2 next " <<thing2->getNext() << endl;
+	thing1->setNext(thing2);
+	std::cout <<"thing1 next "<<thing1->getNext() <<" thing2 next " <<thing2->getNext() << endl;
+	std::cout <<&thing2 <<endl;
+
+
 
 
 	return 0;
