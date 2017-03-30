2a4de7a2f6bbbbad8594433c1e11391c3b640f76
goggalor1954 <goggalor1954@gmail.com>
2017-03-30 15:12:42 -0400

    ok. for some fucking reason it works now just because i put it in public. ill try add fix it with private one more time.

--- HW4/nodex.cpp
+++ HW4/nodex.cpp
@@ -8,11 +8,11 @@ using namespace std;
 class NodeX
 {
 	public:
-	NodeX(void);
-	void setNext(NodeX*& nextPtr);
-	NodeX* getNext(void);
-	float getSalary();
-	void setSalary(float sal);
+	NodeX() {};
+	void setNext(NodeX *nextPtr) {next=nextPtr;}
+	NodeX* getNext(void) {return next;}
+	float getSalary() {return Salary;}
+	void setSalary(float sal) {Salary=sal;}
 	//testing
 	//NodeX& getNextA();
 
@@ -20,31 +20,35 @@ class NodeX
 	NodeX *next;
 	float Salary;
 };
-
+/*
 NodeX::NodeX(void) {next=NULL;}
-void NodeX::setNext(NodeX*& nextPtr) {next=nextPtr;}
+//void NodeX::setNext(NodeX*& nextPtr) {next=&nextPtr;}
+void NodeX::setNext(NodeX *nextPtr) {next =nextPtr;}
 NodeX* NodeX::getNext(void) {return next;}
 //NodeX& NodeX::getNextA(void) {return *next;}
 float NodeX::getSalary() {return Salary;}
 void NodeX::setSalary(float sal) {Salary=sal;}
-
+*/
 int main()
 {
-	NodeX *thing1=new NodeX, *thing2=new NodeX;
-	thing1->setSalary(15.0);
-	thing2->setSalary(3.50);
-	/*std::cout << thing1->getSalary() <<"\n"<<thing2->getSalary() <<endl;
+	NodeX thing1;
+	NodeX thing2;
+	thing1.setSalary(15.0);
+	thing2.setSalary(3.50);
+	std::cout <<thing1.getSalary() <<"\n"<<thing1.getNext() <<endl;
+	thing1.setNext(&thing2);
+	std::cout <<thing1.getNext() <<endl;
+	std::cout <<thing1.getNext()->getSalary() <<endl;
 	
-	thing1->setNext(*thing2);
-	std::cout << thing1->getSalary() <<"\n" << thing1->getNext()->getSalary() << endl;
-	*/
+
+
 	//testing
-	std::cout <<&thing1 << " thin1g address"<< endl;;
-	std::cout <<&thing2 <<" thing2 address" << endl;
-	std::cout <<"Thing1 next " <<(*thing1).getNext() <<" thing2 next " <<thing2->getNext() << endl;
-	thing1->setNext(thing2);
-	std::cout <<"thing1 next "<<thing1->getNext() <<" thing2 next " <<thing2->getNext() << endl;
-	std::cout <<&thing2 <<endl;
+//	std::cout <<&thing1 << " thin1g address"<< endl;;
+//	std::cout <<&thing2 <<" thing2 address" << endl;
+//	std::cout <<"Thing1 next " <<(*thing1).getNext() <<" thing2 next " <<thing2->getNext() << endl;
+//	thing1->setNext(*thing2);
+//	std::cout <<"thing1 next "<<thing1->getNext() <<" thing2 next " <<thing2->getNext() << endl;
+//	std::cout <<&thing2 <<endl;
 
 
 
