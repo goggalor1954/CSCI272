6a61fe1c19dc5efaa0e41a89ac87e031fd52ff18
goggalor1954 <goggalor1954@gmail.com>
2017-03-30 17:44:17 -0400

    almost finished up. just need to found the dollar and then move all of this into a function class.

--- HW5/PureV.cpp
+++ HW5/PureV.cpp
@@ -18,6 +18,8 @@ class PureV {
 		void setAdjSalary(float sal); //sets Adjusted salary
 		virtual void salary(void) =0; //virtual function. increases salary depending on which derived class calls it.
 		//testing
+		float getToZero(float val);
+		float getCents(float val);
 		
 	private:
 		float rawSalary;
@@ -45,6 +47,19 @@ void PureV::setAdjSalary(float sal) {adjSalary=sal;}
 void goodEmployee::salary(void){ setRawSalary(getRawSalary()+.20*getRawSalary());} //increases value of rawSalary by 20%
 void betterEmployee::salary(void){ setRawSalary(getRawSalary()+.30*getRawSalary());} //increases value of rawSalary by 30%
 
+//testing
+float PureV::getToZero(float val)
+{
+	if(val<=1) return 0;
+	return 1+(getToZero(val-1));
+}
+float PureV::getCents(float val)
+{
+	if(val<=.01) return 0;
+	return .01+(getCents(val-.01));
+}
+
+
 
 
 int main()
@@ -52,7 +67,7 @@ int main()
 	//testing
 	goodEmployee frank;
 	betterEmployee george;
-	frank.setRawSalary(6.893);
+	frank.setRawSalary(44.7653);
 	george.setRawSalary(100);
 	std::cout << "Frank's raw salary: " <<frank.getRawSalary() <<endl;
 	std::cout << "George's raw salary: " <<george.getRawSalary() <<endl;
@@ -61,7 +76,17 @@ int main()
 	std::cout << "Frank's raw salary: " <<frank.getRawSalary() <<endl;
 	std::cout << "George's raw salary: " <<george.getRawSalary() <<endl;
 	//testing
-	//std::cout << Round(frank.getRawSalary(),2) <<endl;
+	std::cout <<"should have the same whole numbers as franks raw: " << frank.getToZero(frank.getRawSalary()) << endl;
+	std::cout <<"decimal values. " << frank.getRawSalary()-frank.getToZero(frank.getRawSalary()) << endl;
+	std::cout <<"should be the first 2 decimal places " << frank.getCents(frank.getRawSalary()-frank.getToZero(frank.getRawSalary())) <<endl;
+	if(frank.getRawSalary()-frank.getToZero(frank.getRawSalary())-frank.getCents(frank.getRawSalary()-frank.getToZero(frank.getRawSalary())) >=0.005) 
+	{//increase by 1 sent. if it iit increases the dollar value increase that and disregard cents.
+		if(frank.getCents(frank.getRawSalary()-frank.getToZero(frank.getRawSalary()))==.99) {frank
+	}
+	else {frank.setAdjSalary(frank.getToZero(frank.getRawSalary())+frank.getCents(frank.getRawSalary()-frank.getToZero(frank.getRawSalary())));}
+	
+	std::cout << "franks adjusted salary " << frank.getAdjSalary() <<endl;
+	
 
 
 	return 0;
